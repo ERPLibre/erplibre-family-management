b1de0aa48bd4edc18910cc3fbc2d04bc0412f782
Mathieu Benoit <mathben@technolibre.ca>
2022-03-20 11:57:34 -0400

    [ADD] first commit famille_alimentation_nourrisson

---
+++ famille_alimentation_nourrisson/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Famille Alimentation Nourrisson",
+    "category": "Uncategorized",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "application": True,
+    "depends": ["portal"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/famille_alimentation_nourrisson.xml",
+        "views/famille_alimentation_nourrisson_templates.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
+}
